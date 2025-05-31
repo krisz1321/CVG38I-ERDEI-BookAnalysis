07cf7358e221d87d380401609f62d90f8fa1c1bb
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 01:36:29 +0200

    Added book models

---
+++ FRONTEND/BookAnalyzerFrontend/src/app/_models/book_models.ts
@@ -0,0 +1,33 @@
+
+export interface BookDto {
+  id: string;
+  title: string;
+  content: string;
+}
+
+export interface BookTitleDto {
+  id: string;
+  title: string;
+}
+
+export class BookTitles implements BookTitleDto {
+  id: string;
+  title: string;
+
+  constructor() {
+	this.id = '';
+	this.title = '';
+  }
+}
+
+export class Books implements BookDto {
+  id: string;
+  title: string;
+  content: string;
+
+  constructor() {
+	this.id = '';
+	this.title = '';
+	this.content = '';
+  }
+}
