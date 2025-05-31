abd1363ba5d51ad660695521cc026bc5517c00e2
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 01:44:35 +0200

    Integrated list-books route into navigation

--- FRONTEND/BookAnalyzerFrontend/src/app/app.routes.ts
+++ FRONTEND/BookAnalyzerFrontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListBooksComponent } from './list-books/list-books.component';
+
 
 export const routes: Routes = [
 	{path: '', component: HomeComponent},
 	{path: 'home', component: HomeComponent},
-
+	{path: 'list-books', component: ListBooksComponent},
     {path: 'list-words', component: HomeComponent}, // Egyelőre ugyanaz a komponens
 
 	{path: '**', redirectTo: '', pathMatch: 'full'} 
