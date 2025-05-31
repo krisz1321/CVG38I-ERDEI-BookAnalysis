9e4642d4875e2fd47fbcaa28a049873758456ed2
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 01:30:55 +0200

    Implemented navigation menu

--- FRONTEND/BookAnalyzerFrontend/src/app/app.routes.ts
+++ FRONTEND/BookAnalyzerFrontend/src/app/app.routes.ts
@@ -1,11 +1,19 @@
-import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
 	{path: '', component: HomeComponent},
 	{path: 'home', component: HomeComponent},
-	
+
+    {path: 'list-words', component: HomeComponent}, // Egyelőre ugyanaz a komponens
 
 	{path: '**', redirectTo: '', pathMatch: 'full'} 
 ];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- FRONTEND/BookAnalyzerFrontend/src/app/home/home.component.ts
+++ FRONTEND/BookAnalyzerFrontend/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
