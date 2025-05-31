c025ed18bb47cd12480ed744cce17e311ab27f80
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 01:58:04 +0200

    Created Phrase service

--- FRONTEND/BookAnalyzerFrontend/src/app/app.routes.ts
+++ FRONTEND/BookAnalyzerFrontend/src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListBooksComponent } from './list-books/list-books.component';
+import { ListWordsComponent } from './list-words/list-words.component';
 
 
 export const routes: Routes = [
 	{path: '', component: HomeComponent},
 	{path: 'home', component: HomeComponent},
 	{path: 'list-books', component: ListBooksComponent},
-    {path: 'list-words', component: HomeComponent}, // Egyelőre ugyanaz a komponens
+    {path: 'list-words', component: ListWordsComponent}, 
 
 	{path: '**', redirectTo: '', pathMatch: 'full'} 
 ];
