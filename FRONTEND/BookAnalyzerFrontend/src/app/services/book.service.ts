b0cc175f23377e585d57b52accdd533bcae3c665
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 01:41:20 +0200

    Implemented Book service

--- FRONTEND/BookAnalyzerFrontend/src/app/services/book.service.ts
+++ FRONTEND/BookAnalyzerFrontend/src/app/services/book.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Environment } from '../environment/enviroment';
+import { BookDto, BookTitles } from '../_models/book_models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+	http: HttpClient;
 
-  constructor() { }
+  constructor(http: HttpClient) {
+	this.http = http;
+   }
+
+	getBookTitles(): Observable<Array<BookTitles>> {
+		return this.http.get<Array<BookDto>>(`${Environment.apiUrl}/api/Books/GetBookTitles`)
+		.pipe(
+			map(response => 
+			response.map(bookDto => {
+				const book = new BookTitles();
+				book.id = bookDto.id;
+				book.title = bookDto.title;
+				return book;
+			})
+			)
+		);
+	}
+
 }
