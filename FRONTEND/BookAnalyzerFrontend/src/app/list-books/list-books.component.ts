c7cf812938f4a614aea21c4b7a9739426ec2a4d4
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 01:47:46 +0200

    Implemented logic for ListBooksComponent

--- FRONTEND/BookAnalyzerFrontend/src/app/list-books/list-books.component.ts
+++ FRONTEND/BookAnalyzerFrontend/src/app/list-books/list-books.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { NavigationComponent } from "../navigation/navigation.component";
+import { BookDto, BookTitleDto, BookTitles, Books  } from '../_models/book_models';
+import {CommonModule} from '@angular/common';
+import { Environment } from '../environment/enviroment';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-list-books',
-  imports: [],
+  imports: [NavigationComponent, HttpClientModule, CommonModule],
   templateUrl: './list-books.component.html',
-  styleUrl: './list-books.component.scss'
+  styleUrl: './list-books.component.scss',
+  providers: [BookService],
 })
-export class ListBooksComponent {
 
+export class ListBooksComponent implements OnInit {
+
+  books: Array<BookTitles> = [];
+  isLoading: boolean = false;
+  errorMessage: string = '';
+    
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks(): void {
+    this.isLoading = true;
+    this.errorMessage = '';
+    
+    this.bookService.getBookTitles().subscribe(
+      books => {
+        this.books = books;
+        this.isLoading = false;
+        console.log('Books loaded:', this.books);
+      },
+      error => {
+        console.error('Error loading books:', error);
+        this.errorMessage = 'Hiba történt a könyvek betöltése során.';
+        this.isLoading = false;
+      }
+    );
+  }
+
+  retry(): void {
+    this.loadBooks();
+  }
 }
