66dd3349d0736a9647859ebf01d353f0d17edee1
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 01:55:28 +0200

    Created ListWordsComponent

---
+++ FRONTEND/BookAnalyzerFrontend/src/app/list-words/list-words.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-words',
+  imports: [],
+  templateUrl: './list-words.component.html',
+  styleUrl: './list-words.component.scss'
+})
+export class ListWordsComponent {
+
+}
