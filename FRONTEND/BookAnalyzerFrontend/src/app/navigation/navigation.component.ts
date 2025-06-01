5871ade2878b52b03776c5ab8553f271268401bc
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 06:03:09 +0200

    Implemented dynamic navbar visibility

--- FRONTEND/BookAnalyzerFrontend/src/app/navigation/navigation.component.ts
+++ FRONTEND/BookAnalyzerFrontend/src/app/navigation/navigation.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../services/api.service';
+
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule,], 
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
 
+    api: ApiService;
+    constructor(api: ApiService) {
+        this.api = api;
+    }
 }
