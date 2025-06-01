b67dc24ea0294345eb8b2f133702c1dad7b05f8d
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 19:06:46 +0200

    Generated word-manager component for favorite and learned words

---
+++ FRONTEND/BookAnalyzerFrontend/src/app/word-manager/word-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-manager',
+  imports: [],
+  templateUrl: './word-manager.component.html',
+  styleUrl: './word-manager.component.scss'
+})
+export class WordManagerComponent {
+
+}
