c4c4047b2f8b272998070cb15259d5bdaa439733
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 03:36:48 +0200

    Linked Login route to main routing module

--- FRONTEND/BookAnalyzerFrontend/src/app/app.routes.ts
+++ FRONTEND/BookAnalyzerFrontend/src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListBooksComponent } from './list-books/list-books.component';
 import { ListWordsComponent } from './list-words/list-words.component';
-
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
 	{path: '', component: HomeComponent},
 	{path: 'home', component: HomeComponent},
 	{path: 'list-books', component: ListBooksComponent},
     {path: 'list-words', component: ListWordsComponent}, 
+	{path: 'login', component: LoginComponent},
 
 	{path: '**', redirectTo: '', pathMatch: 'full'} 
 ];
