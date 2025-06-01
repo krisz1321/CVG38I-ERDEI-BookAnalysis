b1e8e753b91c7c880ce7c6dbfc0a83143b5725d5
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 04:11:19 +0200

    Integrated login validation and route protection in ApiService

--- FRONTEND/BookAnalyzerFrontend/src/app/services/api.service.ts
+++ FRONTEND/BookAnalyzerFrontend/src/app/services/api.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+	 router: Router
+
+  	constructor(router: Router) { 
+		this.router = router;
+
+	}
+
+	public isLoggedIn() : boolean {
+        let token = localStorage.getItem('bookanalyzer-token')
+        // TODO check expiration date etc.
+        return token !== null;
+    }
+
+	public isAdmin() : boolean {
+        
+        return false; // TODO implement admin check
+    }
+
+
+	public canActivate() : boolean {
+        if (!this.isLoggedIn()) {
+            this.router.navigate(['/login'])
+            return false
+        }
+        return true
+	}
+
 }
