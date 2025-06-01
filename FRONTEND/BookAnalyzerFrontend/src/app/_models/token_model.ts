24762b86f7b3c2b08533a476a71c8846aed939b1
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 03:33:48 +0200

    Defined TokenModel with authentication properties

---
+++ FRONTEND/BookAnalyzerFrontend/src/app/_models/token_model.ts
@@ -0,0 +1,9 @@
+export class TokenModel implements TokenDto {
+    public expiration:Date = new Date()
+    public token:string = ''
+}
+
+export interface TokenDto {
+	expiration: Date;
+	token: string;
+}
