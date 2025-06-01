b78eaa6758dfb6966080767da39e1fb9b56289c0
krisz1321 <erdeikrisz1321@gmail.com>
2025-06-01 03:38:38 +0200

    Created authentication models for login and user management

---
+++ FRONTEND/BookAnalyzerFrontend/src/app/_models/login_models.ts
@@ -0,0 +1,25 @@
+export class LoginModel implements LoginDto {
+    public userName:string = ""
+    public password:string = ""
+}
+
+export interface LoginDto {
+  userName: string;
+  password: string;
+}
+
+
+export interface UserInputDto {
+  userName: string;
+  password: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+export interface UserViewDto {
+  id: string;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  isAdmin: boolean;
+}
